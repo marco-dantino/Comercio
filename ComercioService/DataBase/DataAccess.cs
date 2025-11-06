83d9dc150cd4cc3ac2569d641f8a1c2a7319f728
Marco D' Antino <marco.dantino@colidevs.com>
2025-11-06 16:27:51 -0300

    Arquitectura incial

---
+++ ComercioService/DataBase/DataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComercioService.DataBase
+{
+    internal class DataAccess
+    {
+    }
+}
