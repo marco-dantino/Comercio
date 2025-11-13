d674b2cdb0f39ec54624caff2568f8fe48b8368d
Marco D' Antino <marco.dantino@colidevs.com>
2025-11-13 00:32:13 -0300

    service essential

---
+++ ComercioService/Service/ServiceCliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComercioService.Service
+{
+    public class ServiceCliente
+    {
+    }
+}
